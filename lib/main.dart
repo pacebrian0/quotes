7b64bcde677e2e76925512dfbbdd311f71f3bee1
Brian Pace <pacebrian0@gmail.com>
2023-03-18 19:54:32 +0100

    class

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quotes/quote.dart';
+import 'quote.dart';
 
 void main() {
   runApp(MaterialApp(
     home: QuoteList(),
-
   ));
 }
-
 
 class QuoteList extends StatefulWidget {
   const QuoteList({Key? key}) : super(key: key);
@@ -16,26 +16,23 @@ class QuoteList extends StatefulWidget {
 }
 
 class _QuoteListState extends State<QuoteList> {
-  List<String> quotes = [
-     'Be yourself; everyone else is already taken',
-    'I have nothing to declare except my genius',
-    'The truth is rarely pure and never simple'
+    List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey.shade200,
-    appBar: AppBar(
-      title: Text("Awesome Quotes"),
-      centerTitle: true,
-      backgroundColor: Colors.redAccent,
-    ),
-      body: Column(
-        children: quotes.map((quote) => Text(quote)).toList(),
-      )
-
-    );
+        backgroundColor: Colors.grey.shade200,
+        appBar: AppBar(
+          title: Text("Awesome Quotes"),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          children: quotes.map((quote) => Text("${quote.text} ~ ${quote.author}")).toList(),
+        ));
   }
 }
-
